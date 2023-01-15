b3c1aa0c1f079704eecddfe771a8ba90c0c905b4
maxtkc <maxtkc@users.noreply.github.com>
2023-01-15 04:31:09 +0000

    jekyll build from Action 73b2b1f86ec8841dcf9e9283a596ee3d1246d95f

---
+++ sw.js
@@ -0,0 +1,73 @@
+const version = '20230115043107';
+const cacheName = `static::${version}`;
+
+const buildContentBlob = () => {
+  return ["/mos-herb-site/jekyll/update/2022/12/30/welcome-to-jekyll.html","/mos-herb-site/about.html","/mos-herb-site/contact.html","/mos-herb-site/","/mos-herb-site/assets/styles.css","/mos-herb-site/work_w_me.html","/mos-herb-site/manifest.json","/mos-herb-site/assets/search.json","/mos-herb-site/feed.xml","/mos-herb-site/assets/images/big_red_flower.jpg", "/mos-herb-site/assets/default-offline-image.png", "/mos-herb-site/assets/scripts/fetch.js"
+  ]
+}
+
+const updateStaticCache = () => {
+  return caches.open(cacheName).then(cache => {
+    return cache.addAll(buildContentBlob());
+  });
+};
+
+const clearOldCache = () => {
+  return caches.keys().then(keys => {
+    // Remove caches whose name is no longer valid.
+    return Promise.all(
+      keys
+        .filter(key => {
+          return key !== cacheName;
+        })
+        .map(key => {
+          console.log(`Service Worker: removing cache ${key}`);
+          return caches.delete(key);
+        })
+    );
+  });
+};
+
+self.addEventListener("install", event => {
+  event.waitUntil(
+    updateStaticCache().then(() => {
+      console.log(`Service Worker: cache updated to version: ${cacheName}`);
+    })
+  );
+});
+
+self.addEventListener("activate", event => {
+  event.waitUntil(clearOldCache());
+});
+
+self.addEventListener("fetch", event => {
+  let request = event.request;
+  let url = new URL(request.url);
+
+  // Only deal with requests from the same domain.
+  if (url.origin !== location.origin) {
+    return;
+  }
+
+  // Always fetch non-GET requests from the network.
+  if (request.method !== "GET") {
+    event.respondWith(fetch(request));
+    return;
+  }
+
+  // Default url returned if page isn't cached
+  let offlineAsset = "/offline/";
+
+  if (request.url.match(/\.(jpe?g|png|gif|svg)$/)) {
+    // If url requested is an image and isn't cached, return default offline image
+    offlineAsset = "/mos-herb-site/assets/default-offline-image.png";
+  }
+
+  // For all urls request image from network, then fallback to cache, then fallback to offline page
+  event.respondWith(
+    fetch(request).catch(async () => {
+      return (await caches.match(request)) || caches.match(offlineAsset);
+    })
+  );
+  return;
+});
